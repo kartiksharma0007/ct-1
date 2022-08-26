b3db062b36cdb8672ef1c0d7baf16ed83f9db196
KARTIK SHARMA <78273721+kartiksharma0007@users.noreply.github.com>
2022-08-26 13:44:41 +0530

    Add files via upload
    Class Test 1

---
+++ q1.cpp
@@ -0,0 +1,48 @@
+// Kartik Sharma
+// 2010990371
+// Set-3
+// Rearrange The Array With Alternate High And Low Elements.
+
+#include <iostream>
+using namespace std;
+  
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+ 
+void rearrangeArray(int arr[], int n)
+{
+    
+    for (int i = 1; i < n; i += 2)
+    {
+        if (arr[i - 1] > arr[i]) {
+            swap(arr, i - 1, i);
+        }
+        if (i + 1 < n && arr[i + 1] > arr[i]) {
+            swap(arr, i + 1, i);
+        }
+    }
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+   for(int i=0;i<n;i++)
+   {
+       cin>>arr[i];
+   }
+ 
+    rearrangeArray(arr, n);
+ 
+    
+    for (int i = 0; i < n; i++) {
+        cout<< arr[i]<<" ";
+    }
+ 
+    return 0;
+}
